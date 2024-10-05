52d96453e39aec30483ada84d4ab7e086a34fee6
St Cyr Vandersar <leejvan02@gmail.com>
2024-10-04 20:40:14 -0400

    [add]: add routes in app

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:events_ticket/presentation/screens/onboding/onboarding_screen.dart';
+// import 'package:events_ticket/presentation/screens/onboding/onboarding_screen.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
