ef0719117043cc0387d17918742a6408d220ce77
St Cyr Vandersar <leejvan02@gmail.com>
2024-10-24 15:27:31 -0400

    [update]: updating qr code generator and scan

--- lib/presentation/screens/events/qr_scanner_screen.dart
+++ lib/presentation/screens/events/qr_scanner_screen.dart
@@ -103,7 +103,7 @@ class QRScannerScreenState extends State<QRScannerScreen> {
                 if (qrCodeData != null)
                   Text(
                     isValid
-                        ? '$qrCodeData data is valid.'
+                        ? '$qrCodeData'
                         : "QR Code is invalid or tampered with.",
                     style: TextStyle(
                         fontSize: 18,
@@ -145,35 +145,6 @@ class QRScannerScreenState extends State<QRScannerScreen> {
   void dispose() {
     controller.dispose();
     super.dispose();
-  }
-}
-
-// Function to decrypt and verify the QR data
-bool decryptAndVerifyQrData(String? encryptedData, String secretKey) {
-  final key = encrypt.Key.fromUtf8(secretKey);
-  final iv = encrypt.IV.fromLength(16);
-  final encrypter = encrypt.Encrypter(encrypt.AES(key));
-
-  try {
-    // Step 1: Decrypt the data
-    final decrypted = encrypter.decrypt64((encryptedData).toString(), iv: iv);
-
-    // Step 2: Separate ticket data from the signature
-    var parts = decrypted.split("|signature=");
-    if (parts.length != 2) return false;
-
-    var ticketData = parts[0];
-    var signature = parts[1];
-
-    // Step 3: Recalculate the signature
-    var bytes = utf8.encode(ticketData + secretKey);
-    var recalculatedSignature = sha256.convert(bytes).toString();
-
-    // Step 4: Compare the recalculated signature with the original one
-    return recalculatedSignature == signature;
-  } catch (e) {
-    // If decryption or verification fails, return false
-    return false;
   }
 }
 
